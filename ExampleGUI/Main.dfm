99e1240b69bb1ca83609b2db19b83ff10d67a638
Kalliduz <kalliduz.dev@gmail.com>
2022-10-11 19:57:30 +0200

    Simple Sample GUI.. WIP REFS #21

---
+++ ExampleGUI/Main.dfm
@@ -0,0 +1,34 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 411
+  ClientWidth = 852
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  OnCreate = FormCreate
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Image1: TImage
+    Left = 32
+    Top = 16
+    Width = 385
+    Height = 387
+  end
+  object Image2: TImage
+    Left = 423
+    Top = 16
+    Width = 194
+    Height = 387
+  end
+  object Timer1: TTimer
+    OnTimer = Timer1Timer
+    Left = 656
+    Top = 104
+  end
+end
---
+++ ExampleGUI/Main.pas
@@ -0,0 +1,60 @@
+﻿unit Main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,GameControl,Display,DataTypes,
+  Vcl.ExtCtrls;
+
+type
+  TForm1 = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Timer1: TTimer;
+    procedure FormCreate(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private-Deklarationen }
+    FGameManager:TGameManager;
+    procedure DisplayLoop;
+  public
+    { Public-Deklarationen }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+{ TForm1 }
+
+procedure TForm1.DisplayLoop;
+var
+  X,Y:SmallInt;
+  tmp:Int64;
+  LGameInfo:TGameInformation;
+begin
+
+  LGameInfo:=FGameManager.GetGameInformation;
+  PaintEmptyBoard(Image1,BOARD_SIZE);
+  PaintOccupation(Image1,BOARD_SIZE,FGameManager.GetBoard());
+  DisplayGameInformation(LGameInfo,Image2);
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  FGameManager:=TGameManager.Create;
+  FGameManager.NewGame(100,100,30,3,False,False);
+  FGameManager.Think;
+end;
+
+procedure TForm1.Timer1Timer(Sender: TObject);
+begin
+  if Assigned(FGameManager) then
+    DisplayLoop;
+end;
+
+end.
