04941906e4037d588ddd5dbf77b4995131d1ea75
Kalliduz <kalliduz.dev@gmail.com>
2025-05-16 22:53:56 +0200

    implemented simple on-click logic

--- ExampleGUI/Main.dfm
+++ ExampleGUI/Main.dfm
@@ -19,12 +19,21 @@ object Form1: TForm1
     Top = 16
     Width = 385
     Height = 387
+    OnMouseDown = Image1MouseDown
   end
   object Image2: TImage
     Left = 423
     Top = 16
     Width = 194
     Height = 387
+  end
+  object Button1: TButton
+    Left = 640
+    Top = 16
+    Width = 75
+    Height = 25
+    Caption = 'Best Move'
+    OnClick = Button1Click
   end
   object Timer1: TTimer
     OnTimer = Timer1Timer
--- ExampleGUI/Main.pas
+++ ExampleGUI/Main.pas
@@ -4,16 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs,GameControl,Display,DataTypes,
-  Vcl.ExtCtrls;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, GameControl, Display, DataTypes,
+  Vcl.ExtCtrls, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
     Image1: TImage;
     Image2: TImage;
     Timer1: TTimer;
+    Button1: TButton;
     procedure FormCreate(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
+    procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Button1Click(Sender: TObject);
   private
     { Private-Deklarationen }
     FGameManager:TGameManager;
@@ -55,6 +59,24 @@ procedure TForm1.Timer1Timer(Sender: TObject);
 begin
   if Assigned(FGameManager) then
     DisplayLoop;
+end;
+
+procedure TForm1.Image1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  BX,BY:Integer;
+begin
+  if Button = mbLeft then
+  begin
+    DisplayToBoard(X,Y,Image1,BOARD_SIZE,BX,BY);
+    if (BX>=1) and (BX<=BOARD_SIZE) and (BY>=1) and (BY<=BOARD_SIZE) then
+      FGameManager.MoveNow(BX,BY);
+  end;
+end;
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  FGameManager.ComputerMoveNow;
 end;
 
 end.
